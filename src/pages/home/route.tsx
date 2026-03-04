098c05104c1dcba28d77c949d561de8297766d30
Deepsayan Das <deepsayandas274@gmail.com>
2026-03-04 10:59:30 +0530

    Revert "added sponsors section and faq section"

--- src/pages/home/route.tsx
+++ src/pages/home/route.tsx
@@ -1,7 +1,5 @@
 import About from "./sections/about/About"
 import { Dashboard } from "./sections/dashboard/Dashboard"
-import Events from "./sections/events/Events"
-import FAQ from "./sections/faq/Faq"
 import Footer from "./sections/footer/Footer"
 import MeetOurTeam from "./sections/meet-our-team/MeetOurTeam"
 import WhySection from "./sections/whySection/WhySection"
@@ -9,15 +7,11 @@ import WhySection from "./sections/whySection/WhySection"
 export const HomeRoute = () => {
 	return (
 		<div className="w-full min-h-screen bg-[#131313]">
-			<Dashboard />
-			<WhySection />
-			<Events />
-			<div className="h-[10vh] md:h-[30vh]" />
-			<About />
-			<FAQ />
-			{/* <MeetOurTeam /> */}
-			<Footer />
-
+		<Dashboard /> 
+		<About/>
+		<WhySection/>
+		<MeetOurTeam/>
+		<Footer />
 		</div>
 	)
 }
